f587b3a9400c483eb547b98f1efbf0de16dbe98c
ymoshk <ymsd133@gmail.com>
2021-02-21 10:39:43 +0200

    Yotam Changes

--- Server/src/server/servlet/json/template/model/rowing/activity/RowingActivityJson.java
+++ Server/src/server/servlet/json/template/model/rowing/activity/RowingActivityJson.java
@@ -7,9 +7,11 @@ import server.servlet.json.template.model.request.RequestJson;
 public class RowingActivityJson {
     public final RequestJson request;
     public final BoatJson boat;
+    public final String id;
 
     public RowingActivityJson(RowingActivity activity) {
         this.request = new RequestJson(activity.getRequest());
         this.boat = new BoatJson(activity.getBoat());
+        this.id = activity.getId();
     }
 }
